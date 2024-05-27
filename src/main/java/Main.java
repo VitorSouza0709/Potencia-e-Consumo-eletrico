f54d18a28c15736eb35e5f5d94d684f209cb340d
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-05-27 17:50:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa para calcular o consumo elétrico ");
+    System.out.println();
+    System.out.println("Uma tv de 200w fica ligada por aproximadamente 5h por dia, Determine o consumo elétrico mensal da tv");
+    System.out.println();
+    System.out.println("o valor da potência do aparelho é de:");
+    int potencia = 200;
+    System.out.println(potencia);
+    System.out.println();
+    System.out.println("o tempo de uso do aparelho é de:");
+    int tempo = 5;
+    System.out.println(tempo);
+    System.out.println();
+    int mes = 30;
+    int consumo = potencia * tempo * mes /1000;
+    System.out.println("o consumo da tv é de:");
+    System.out.println(consumo);
+  }
+
+  
+}
